54bfa6b8db644c3c3f5b9afb19d97b984cca3d12
dayday-13 <dayellenn.souza@escola.pr.gov.br>
2026-03-17 08:13:24 -0300

    Add files via upload

---
+++ usandobreak.js
@@ -0,0 +1,7 @@
+const ferramentas=["martelo", "fita isolante","alicate", "trena","chave fenda"]
+for (let i=0; i< ferramentas.length; i++){
+    console.log("vericado: "+ ferramentas [i]);
+    if (ferramentas [i]=== "chave"){
+        console.log("*** achei a ferramenta! PARANDO A BUSCA")
+    }
+}
